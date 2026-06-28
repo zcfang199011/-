786e35a2b5183c793cfe7ffab6797e826c5ed2d3
zcfang199011 <zcfang1990@gmail.com>
2026-06-28 23:09:58 +0800

    更新 backend-client.js

--- backend-client.js
+++ backend-client.js
@@ -417,7 +417,7 @@ async function backendResetNow() {
     }).catch(() => {});
   });
 
-  if (api.user && api.token) {
+ if (api.user && api.token) {
   renderBadge();
   applyRoleAccess();
   connectSocket();
@@ -431,5 +431,7 @@ async function backendResetNow() {
   });
 } else {
   renderLogin();
+}
+})();
 }
 })();
